5ed331e7240e5afb3904ed7123877979d7352d5b
BRANDON CASTRO BRENES <118040182@castrocarazo.ac.cr>
2021-07-30 18:37:13 -0600

    wasd

--- MPGC_APP/Tools/ObjetosGlobales.cs
+++ MPGC_APP/Tools/ObjetosGlobales.cs
@@ -7,8 +7,8 @@ namespace MPGC_APP.Tools
     public static class ObjetosGlobales
     {
         //Rutas de consumo (Producción y pruebas)
-        public static string RutaProduccion = "http://192.168.159.1:45455/api/";
-        public static string RutaPruebas = "http://192.168.159.1:45455/api/";
+        public static string RutaProduccion = "https://mpgcapi20210730165803.azurewebsites.net/api/";
+        public static string RutaPruebas = "https://mpgcapi20210730165803.azurewebsites.net/api/";
 
         //Seguridad ya sea JWT o ApiKey
         public static string ApiKeyName = "ApiKey";
